bee5a4e4330b4be7627ff2f1c181a5708dfe2e80
JuneHyung <cjh951114@gmail.com>
2021-08-19 18:50:47 +0900

    Create First Project : Setting
    Create First Project : Setting

---
+++ code/do-it-example/src/01/RCC.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RCC extends Component {
+  render() {
+    let text = '따옴표';
+    return (
+      <div name="name">
+        { text}
+      </div>
+    );
+  }
+}
+
+export default RCC;
