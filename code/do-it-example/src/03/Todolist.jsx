5093df5764f40b289cf1e178c1b0b3913a80ea1f
JuneHyung <cjh951114@gmail.com>
2021-08-23 17:15:31 +0900

    Create 03-08 : 배열 컴포넌트
    Create 03-08 : 배열 컴포넌트

---
+++ code/do-it-example/src/03/Todolist.jsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+
+class Todolist extends PureComponent {
+  render() {
+    const todoList = [
+        {taskName : '빨래하기', finished: false},
+        {taskName : '공부하기', finished: true},
+	  ];  
+    return (
+      <div>
+        {todoList.map((todo)=><div key={todo.taskName}>{todo.taskName}</div>)}
+      </div>
+    );
+  }
+}
+
+export default Todolist;
